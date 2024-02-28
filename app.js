7a22cf32802215c6efba5e23fcdd4306310c91c1
EGsagon <egsagon12@gmail.com>
2024-02-28 15:10:25 +0100

    update

---
+++ app.js
@@ -0,0 +1,36 @@
+const api = 'https://api.github.com/users/Egsagon/repos?per_page=100&sort=updated'
+const template = $('template').html()
+
+const repl = (tem, dict) => {
+    for (let [key, value] of Object.entries(dict)) {
+        tem = tem.replace(`{{ ${key} }}`, value)
+    }
+    return tem
+}
+
+$('#ds').on('click', () => {
+    navigator.clipboard.writeText('Egsagon')
+    alert('Discord: Egsagon (copied)')
+})
+
+$.getJSON(api, data => {
+    data.forEach(repo => {
+        if (repo.id === 761968911)
+            repo.html_url = 'https://github.com/EchterAlsFake/PHUB'
+        
+        repo.stars = repo.stargazers_count ? '[' + repo.stargazers_count + '★]' : ''
+
+        $('#projects').append(repl(template, repo))
+    })
+
+    clearInterval(loader)
+    $('#loader').css('display', 'none')
+})
+
+var i = 0
+var loader = setInterval(() => {
+    i++
+    $('#load').html('/-\\|'[i % 4])
+}, 300)
+
+// EOF
